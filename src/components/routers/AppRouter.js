82f26433fc7ebbd2d2bd3f9634bf0ffac225b62a
Matthew Greco <mgreco2112@tutanota.com>
2022-03-07 21:49:41 -0500

    updated home page and added navbar with routes

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Route, Routes} from 'react-router-dom';
 import Home from "../home/Home";
-
+import Navbar from "../navbar/Navbar";
 
 const AppRouter = () => {
     return (
-        <div style={{width: "100%"}}>
+        <div style={{width: "100%", flexDirection: "column"}}>
             {/* Any components that should always be visable should go here */}
-            {/* Navbar */}
+            <Navbar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
             </Routes>
