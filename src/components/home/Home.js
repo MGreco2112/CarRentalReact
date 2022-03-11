7643ce182d32c6ea4df542f21e4a8c3d19cc3346
Matthew Greco <mgreco2112@tutanota.com>
2022-03-10 21:18:56 -0500

    updated frontend, started auth

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Container from "../common/Container";
+import Splash from "../common/Splash";
+import splashImg from "../../assets/splash.jpg"
 
 const Home = () => {
     return (
         <Container>
-            <h1>Welcome to Expression Car Rental!</h1>
-            <h2>All cars have coffee holders</h2>
+            <Splash
+            image = {splashImg}
+            style={{
+                color: '#F1F1F1',
+            }}
+            >
+                <h1
+                style={{
+                    textShadow: '1px, 1px, black'
+                }}
+                >
+                    Welcome to Expression Car Rental!</h1>
+                <h2
+                style={{
+                    textShadow: '1px, 1px, black'
+                }}
+                >All cars have coffee holders</h2>
+            </Splash>
         </Container>
     )
 }
